0e592f6d1ed7215a942de89423b4de2f51e4956c
nickwbarber <nickwbarber@gmail.com>
2023-05-22 19:55:38 +0800

    Use a setter function instead

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   useEffect(() => {
     getAll()
     .then(returnedPersons => {
-      personsState.setter(returnedPersons)
+      personsState.setter(() => returnedPersons)
     })
   }, [])
 
